481125313c1b29097c9ecfe31ebbf8def856bdcd
Kovács Dorina <kovacsdorina2@gmail.com>
2022-11-09 08:54:08 +0100

    működik

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gyepes';
+  title = 'Kör alakú telep terület számítása';
+  // title= "Kör területe";
+  radius !: number;
+  area !: number ;
+  sum !: number;
+
+  calcArea():void{
+    this.area = (Math.pow(this.radius,2)*Math.PI)/2;
+    this.radius = 0;
+
+    this.sum = this.area * 2500;
+  }
 }
